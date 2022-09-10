e28f95e2900dc99a2368356b54a51e426012aa2a
devsunset <devsunset@gmail.com>
2022-09-10 17:39:28 +0900

    dev

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,68 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-
-import { changeField, initializeForm, login } from '../modules/auth';
-import { check } from '../modules/user';
 
 function Login() {
-  const [error, setError] = useState(null);
-  const dispatch = useDispatch();
-  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
-    form: auth.login,
-    auth: auth.auth,
-    authError: auth.authError,
-    user: user.user,
-  }));
-  // 인풋 변경 이벤트 핸들러
-  const onChange = (e) => {
-    const { value, name } = e.target;
-    dispatch(
-      changeField({
-        form: 'login',
-        key: name,
-        value,
-      }),
-    );
-  };
-
-  // 폼 등록 이벤트 핸들러
-  const onSubmit = (e) => {
-    e.preventDefault();
-    alert('aaa');
-    const { username, password } = form;
-    dispatch(login({ username, password }));
-  };
-
-  // 컴포넌트가 처음 렌더링 될 때 form 을 초기화함
-  useEffect(() => {
-    dispatch(initializeForm('login'));
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (authError) {
-      console.log('오류 발생');
-      console.log(authError);
-      setError('로그인 실패');
-      return;
-    }
-    if (auth) {
-      console.log('로그인 성공');
-      dispatch(check());
-    }
-  }, [auth, authError, dispatch]);
-
-  useEffect(() => {
-    if (user) {
-      try {
-        localStorage.setItem('user', JSON.stringify(user));
-      } catch (e) {
-        console.log('localStorage is not working');
-      }
-    }
-  }, [user]);
   return (
     <div>
       <div className="d-flex align-items-center auth px-0">
@@ -89,9 +29,7 @@ function Login() {
                   />
                 </Form.Group>
                 <div className="mt-3">
-                  <Button style={{ width: '240px' }} onClick={onSubmit}>
-                    SIGN IN
-                  </Button>
+                  <Button style={{ width: '240px' }}>SIGN IN</Button>
                 </div>
                 <div className="text-center mt-4 font-weight-light">
                   Don't have an account?{' '}
--- src/index.js
+++ src/index.js
@@ -9,51 +9,16 @@ import './assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0';
 import './assets/css/demo.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer, { rootSaga } from './modules';
-import { tempSetUser, check } from './modules/user';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware)),
-);
-
-function loadUser() {
-  try {
-    const user = localStorage.getItem('user');
-    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
-
-    store.dispatch(tempSetUser(user));
-    store.dispatch(check());
-  } catch (e) {
-    console.log('localStorage is not working');
-  }
-}
-
-sagaMiddleware.run(rootSaga);
-loadUser();
-
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route
-          path="/gotogether"
-          render={(props) => <AdminLayout {...props} />}
-        />
-        <Redirect from="/" to="/gotogether/home" />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Switch>
+      <Route
+        path="/gotogether"
+        render={(props) => <AdminLayout {...props} />}
+      />
+      <Redirect from="/" to="/gotogether/home" />
+    </Switch>
+  </BrowserRouter>,
 );
-
-serviceWorker.unregister();
-
-serviceWorker.unregister();
